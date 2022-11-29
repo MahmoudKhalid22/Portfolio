2f0779b5de38b61e88b8c0d1363d85f80cac32b9
mahmoud khalid <mahmoud0122549@gmail.com>
2022-11-29 14:40:28 +0200

    footer adjusting size

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const p = document.querySelectorAll(".info > p");
 const header = document.querySelector("header .container");
 const cssIcon = document.querySelector(".fa-css3-alt");
 const project = document.querySelector(".projects");
-const phone = document.querySelector(".contact .info h3");
+const phone = document.querySelectorAll(".info> h3");
 
 dark.addEventListener("click", function () {
   document.body.style.backgroundColor = "rgba(0, 0, 0, 0.86)";
@@ -15,11 +15,14 @@ dark.addEventListener("click", function () {
     card[i].style.backgroundColor = "black";
     card[i].style.boxShadow = "0 5px 5px rgba(1 1 1 / 15%)";
     p[i].style.color = "white";
+    phone[i].style.color = "white";
   }
+  // for (let j = o; i < phone.length; j++) {
+  //   phone[j].style.color = "white";
+  // }
   header.style.backgroundColor = "rgb(36, 36, 36)";
   cssIcon.style.color = "white";
   project.style.backgroundColor = "#000016";
-  phone.style.color = "white";
 });
 
 light.addEventListener("click", function () {
@@ -29,9 +32,12 @@ light.addEventListener("click", function () {
   for (let i = 0; i < card.length; i++) {
     card[i].style.backgroundColor = "white";
     p[i].style.color = "black";
+    phone[i].style.color = "black";
   }
+  // for (let j = o; i < phone.length; j++) {
+  //   phone[j].style.color = "black";
+  // }
   cssIcon.style.color = "blue";
   header.style.backgroundColor = "#f0f0f0";
   project.style.backgroundColor = "#ececec";
-  phone.style.color = "black";
 });
