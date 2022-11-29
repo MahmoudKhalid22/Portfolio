9df89fa6064ee3c197dc954f71bfb08587aca9e9
mahmoud khalid <mahmoud0122549@gmail.com>
2022-11-29 14:22:03 +0200

    edit project

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const card = document.querySelectorAll(".card");
 const p = document.querySelectorAll(".info > p");
 const header = document.querySelector("header .container");
 const cssIcon = document.querySelector(".fa-css3-alt");
+const project = document.querySelector(".projects");
+const phone = document.querySelector(".contact .info h3");
 
 dark.addEventListener("click", function () {
   document.body.style.backgroundColor = "rgba(0, 0, 0, 0.86)";
@@ -14,7 +16,10 @@ dark.addEventListener("click", function () {
     card[i].style.boxShadow = "0 5px 5px rgba(1 1 1 / 15%)";
     p[i].style.color = "white";
   }
+  header.style.backgroundColor = "rgb(36, 36, 36)";
   cssIcon.style.color = "white";
+  project.style.backgroundColor = "#000016";
+  phone.style.color = "white";
 });
 
 light.addEventListener("click", function () {
@@ -26,4 +31,7 @@ light.addEventListener("click", function () {
     p[i].style.color = "black";
   }
   cssIcon.style.color = "blue";
+  header.style.backgroundColor = "#f0f0f0";
+  project.style.backgroundColor = "#ececec";
+  phone.style.color = "black";
 });
